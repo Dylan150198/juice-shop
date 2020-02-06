bf53d41411bcd1b4b10dfa1825afd37609042ea4
Kevin <kevinkloet@outlook.com>
2020-02-06 16:45:47 +0100

    fixed sql injection in product search

--- routes/search.js
+++ routes/search.js
@@ -6,6 +6,10 @@ module.exports = function searchProducts () {
   return (req, res, next) => {
     let criteria = req.query.q === 'undefined' ? '' : req.query.q || ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    let regex = /^[a-zA-Z0-9]*$/
+    if(!regex.test(criteria)) {
+      criteria = '///ForcedNoProductFound///';
+    }
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
       .then(([products]) => {
         const dataString = JSON.stringify(products)
